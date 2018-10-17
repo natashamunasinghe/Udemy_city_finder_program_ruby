0724762a6a9837b469d4ea8358492ae8c27f69df
Your Name <you@example.com>
2018-10-18 09:28:18 +1100

    complete project app

--- city_finder.rb
+++ city_finder.rb
@@ -10,63 +10,66 @@
 
 #Hash
 destination = {
-"Sydney" => "2000",
-"Melbourne" => "3000",
-"Brisbane" => "4000",
-"Adelaide" => "5000",
-"Perth" => "6000",
-"Darwin" => "0800",
-"Hobart" => "7000"
+:Sydney => "2000",
+:Melbourne => "3000",
+:Brisbane => "4000",
+:Adelaide => "5000",
+:Perth => "6000",
+:Darwin => "0800",
+:Hobart => "7000"
 }
 
-# def city_info
-#   @cities = destination.select{ |k,v| k == "sydney" }
-# end
 
-#if/else statement and loops
+#if/else statements and loops
 
-puts "Do you want to look for up a postcode? (Y/N)"
-response = gets.chomp.capitalize
+loop do
+  puts "Do you want to look for up a postcode? (Y/N)"
+  response = gets.chomp.capitalize
 
-if response == "Y"
-  puts "Your choices are #{destination.keys}"
-  sleep(1.0)
+  if response == "Y"
+    puts "Your choices are #{destination.keys}"
+    sleep(0.5)
 
-elsif response == "N"
-  puts "See you next time!"
+    puts "Which city postcode from the list above are you looking for?"
+    city_choice = gets.chomp.capitalize
 
-else
-  puts "Invalid response"
+    if city_choice == "Sydney"
+      puts "The postcode for Sydney is #{destination.fetch(:Sydney)}"
+    
+    elsif city_choice == "Melbourne"
+     puts "The postcode for Sydney is #{destination.fetch(:Melbourne)}"
 
-puts "Which city postcode from the list above are you looking for?"
-city_choice = gets.chomp.capitalize
-
-  if city_choice == "Sydney"
-    puts "The postcode for Sydney is #{destination.fetch("Sydney")}"
-   
-  elsif city_choice == "Melbourne"
-    puts "The postcode for Sydney is #{destination.fetch("Melbourne")}"
-
-  elsif city_choice == "Brisbane"
-    puts "The postcode for Brisbane is #{destination.fetch("Brisbane")}"
+    elsif city_choice == "Brisbane"
+      puts "The postcode for Brisbane is #{destination.fetch(:Brisbane)}"
   
-  elsif city_choice == "Adelaide"
-    puts "The postcode for Adelaide is #{destination.fetch("Adelaide")}"
+    elsif city_choice == "Adelaide"
+      puts "The postcode for Adelaide is #{destination.fetch(:Adelaide)}"
 
-  elsif city_choice == "Perth"
-    puts "The postcode for Perth is #{destination.fetch("Perth")}"
+    elsif city_choice == "Perth"
+      puts "The postcode for Perth is #{destination.fetch(:Perth)}"
 
-  elsif city_choice == "Darwin"
-    puts "The postcode for Darwin is #{destination.fetch("Darwin")}"
+    elsif city_choice == "Darwin"
+      puts "The postcode for Darwin is #{destination.fetch(:Darwin)}"
 
-  elsif city_choice == "Hobart"
-    puts "The postcode for Hobart is #{destination.fetch("Hobart")}"
+    elsif city_choice == "Hobart"
+      puts "The postcode for Hobart is #{destination.fetch(:Hobart)}"
 
-  else
+    else
+      puts "Invalid response"
+  
+end
+
+  elsif response == "N"
+      puts "See you next time"
+  
+break
+  
+ else
     puts "Invalid response"
-  
+    
   end
-end 
+
+    end 
 
 
 
