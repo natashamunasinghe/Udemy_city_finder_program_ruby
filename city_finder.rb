8b4b1e816462c7ddadf475164dc0e8451ac54b61
Your Name <you@example.com>
2018-10-17 11:52:22 +1100

    add personalapplication page

---
+++ city_finder.rb
@@ -0,0 +1,25 @@
+###PERSONAL PROJECT###
+
+###CODE###
+
+#1. Create hash with city names and postcodes
+#2. Create method to access too city names
+#3. Create method to
+#4. Get user input 
+#4. Create a loop to continue running the program
+
+#Hash
+
+destination = {
+"Sydney" => "2000",
+"Melbourne" => "3000",
+"Brisbane" => "4000",
+"Adelaide" => "5000",
+"Perth" => "6000",
+"Darwin" => "0800",
+"Hobart" => "7000"
+}
+
+#
+
+
