bd5e67aa5e5d8bd9de2c1cfa4e7ec82151431983
Your Name <you@example.com>
2018-10-17 14:16:56 +1100

    add if else statement

--- city_finder.rb
+++ city_finder.rb
@@ -3,13 +3,12 @@
 ###CODE###
 
 #1. Create hash with city names and postcodes
-#2. Create method to access too city names
-#3. Create method to
-#4. Get user input 
-#4. Create a loop to continue running the program
+#2. Get user input 
+#3. Display names to user which are available (Using a method)
+#4. Display available area codes to user (Using a method)
+#5. Create a loop to continue running the program
 
 #Hash
-
 destination = {
 "Sydney" => "2000",
 "Melbourne" => "3000",
@@ -19,6 +18,54 @@ destination = {
 "Darwin" => "0800",
 "Hobart" => "7000"
 }
+
+# def city_info
+#   @cities = destination.select{ |k,v| k == "sydney" }
+# end
+
+#if/else statement
+puts "Do you want to look for up a postcode? (Y/N)"
+response = gets.chomp.capitalize
+
+if response == "Y"
+  puts "hello"
+
+elsif response == "N"
+  puts "See you next time!"
+
+else
+  puts "Invalid response"
+
+end
+
+
+
+
+puts "Which city postcode are you looking for?"
+response = gets.chomp
+
+destination.each { |k, v| puts "Your city choices are #{k}" }
+# hash.each { |k, v| puts "The key is #{k} and the value is #{v}" }
+
+# puts "Here are your city choices #{city_info}"
+
+
+# destination.each do |x, v|
+#     puts "The available city locations are #{x}"
+# end
+
+# p destination.map {|x| x.values_at}.uniq
+
+
+
+#
+
+# #method to display names
+# def get_names
+#   destination.
+# end
+
+
 
 #
 
